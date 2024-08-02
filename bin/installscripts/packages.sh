e74164e52126a69875fb640d59abc4fd626298cc
Trevor Baughn <trevor.baughn@protonmail.com>
2024-08-02 11:47:39 -0700

    added flatpaks to packages.sh

--- bin/installscripts/packages.sh
+++ bin/installscripts/packages.sh
@@ -10,6 +10,28 @@ extra=(
   nvim
 )
 
+flatpaks=(
+  com.felipekinoshita.Kana
+  com.github.qarmin.czkawka
+  com.obsproject.Studio
+  com.usebottles.bottles
+  com.slack.slack
+  im.riot.Riot
+  io.github.spacingbat3.webcord
+  io.lmms.LMMS
+  md.obsidian.Obsidian
+  one.ablaze.floorp
+  org.audacityteam.Audacity
+  org.gimp.GIMP
+  org.inkscape.Inkscape
+  org.kde.kdenlive
+  org.kde.krita
+  org.libreoffice.LibreOffice
+  org.nomacs.ImageLounge
+  org.videolan.VLC
+  us.zoom.Zoom
+)
+
 environment=(
   aliases
   base-devel
@@ -95,6 +117,14 @@ environment=(
 
 for PKG1 in "${environment[@]}" "${extra[@]}"; do
   sudo pacman -S --noconfirm "$PKG1" | tee -a "$LOG"
+  if [ $? -ne 0 ]; then
+    echo -e "\e[1A\e[K${ERROR} - $PKG1 Package installation failed, Please check the installation logs"
+    exit 1
+  fi
+done
+
+for PGK1 in "${flatpaks[@]}"; do
+  sudo flatpak install --noninteractive "$PKG1" | tee -a "$LOG"
   if [ $? -ne 0 ]; then
     echo -e "\e[1A\e[K${ERROR} - $PKG1 Package installation failed, Please check the installation logs"
     exit 1
