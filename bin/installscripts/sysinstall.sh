8dba1ae02854775d8fa4dd9473f0f9682731b1e9
Trevor Baughn <trevor.baughn@protonmail.com>
2024-08-02 03:59:55 -0700

    added nemo bookmarks

--- bin/installscripts/sysinstall.sh
+++ bin/installscripts/sysinstall.sh
@@ -8,3 +8,13 @@ loadkeys us
 execute_script "packages.sh"
 
 ln -s /usr/bin/kitty /usr/bin/gnome-terminal #makes certain gnome apps work with kitty
+
+#Create directories for bookmarking
+mkdir ~/Art/
+mkdir ~/Pictures/
+mkdir ~/Videos/
+mkdir ~/Documents/
+mkdir ~/Music/
+mkdir ~/Pictures/Screenshots/
+mkdir ~/GameProjects/
+mkdir ~/Applications/
