bfeee163d21be66ac993040c0e789825ab9bb281
Trevor Baughn <trevor.baughn@protonmail.com>
2024-08-07 21:44:39 -0700

    tidy

--- bin/installscripts/sysinstall.sh
+++ bin/installscripts/sysinstall.sh
@@ -32,14 +32,16 @@ execute_script "packages.sh"
 sudo mkdir /usr/share/themes/
 sudo mkdir /usr/share/icons/
 # Move those directories, whether they existed or not, to $HOME equivalent directores.
-sudo mv /usr/share/themes/ $HOME/.themes/
-sudo mv /usr/share/icons/ $HOME/.icons/
+sudo mv -f /usr/share/themes/ $HOME/.themes/
+sudo mv -f /usr/share/icons/ $HOME/.icons/
+sudo mv -f $HOME/.icons/ $HOME/.local/share/icons/
 # Create a symlink between the new and old paths
 sudo ln -s $HOME/.themes /usr/share/themes
 sudo ln -s $HOME/.icons /usr/share/icons
+sudo ln -s $HOME/.local/share/icons $HOME/.icons
 # Set Flatpaks to have access to those directories, as well as the GTK4 themes in xdg-config.
 sudo flatpak override --user --filesystem=$HOME/.themes
-sudo flatpak override --user --filesystem=$HOME/.icons
+sudo flatpak override --user --filesystem=$HOME/.local/share/icons
 sudo flatpak override --user --filesystem=xdg-config/gtk-4.0
 # Set the GTK Theme and GTK Icons for non-Flatpaks (everforest-gtk-theme-git from AUR)
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
