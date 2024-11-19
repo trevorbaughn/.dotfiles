02826af0178fadf4a6ea3f8bec52d3a58d0f247f
Trevor Baughn <trevor.baughn@protonmail.com>
2024-11-18 16:49:20 -0800

    Update auto-partition.sh

--- bin/installscripts/auto-partition.sh
+++ bin/installscripts/auto-partition.sh
@@ -70,9 +70,9 @@ btrfs subvolume create /mnt/@snapshots
 echo -e "[${Cyan}*${White}] Unmounting root and remounting as btrfs subvolumes"
 umount /mnt
 mkdir -p /mnt/{boot,home,.snapshots}
-mount -o noatime,space_cache,compress=zstd,subvol=@ /dev/$root_partition /mnt
-mount -o noatime,space_cache,compress=zstd,subvol=@home /dev/$root_partition /mnt/home
-mount -o noatime,space_cache,compress=zstd,subvol=@snapshots /dev/$root_partition /mnt/.snapshots
+mount -o noatime,space_cache=v2,compress=zstd,subvol=@ /dev/$root_partition /mnt
+mount -o noatime,space_cache=v2,compress=zstd,subvol=@home /dev/$root_partition /mnt/home
+mount -o noatime,space_cache=v2,compress=zstd,subvol=@snapshots /dev/$root_partition /mnt/.snapshots
 
 if [ -z "$swap" ]; then
   echo -e "[${Cyan}*${White}] Not creating a SWAP partition, no need to swapon"
