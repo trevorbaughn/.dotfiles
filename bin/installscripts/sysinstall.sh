bfa92ba4b79d77ce48ccbd98c977198c3c4933e7
Trevor Baughn <trevor.baughn@protonmail.com>
2024-11-19 01:23:03 -0800

    Update sysinstall.sh

--- bin/installscripts/sysinstall.sh
+++ bin/installscripts/sysinstall.sh
@@ -192,7 +192,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable grub-btrfsd
 
 echo -e "[${Cyan}*${White}] Enabling SDDM"
-#systemctl enable sddm
+systemctl enable sddm
 touch /etc/sddm.conf.d/rootless-wayland.conf
 echo "[General]" >"/etc/sddm.conf.d/rootless-wayland.conf"
 echo "DisplayServer=wayland" >>"/etc/sddm.conf.d/rootless-wayland.conf"
