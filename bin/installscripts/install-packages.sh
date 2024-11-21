f8ad4ade5b7f557d0e8581830d2a09034cbb262f
Trevor Baughn <trevor.baughn@protonmail.com>
2024-11-20 17:18:46 -0800

    Update install-packages.sh

--- bin/installscripts/install-packages.sh
+++ bin/installscripts/install-packages.sh
@@ -55,7 +55,7 @@ else
 fi
 for pkg in "${cpu_pkgs[@]}" "${gpu_pkgs[@]}"; do
   echo -e "$root_password\n" | sudo -S -v
-  sudo -i echo "$pkg" >>./packages/hardware-specific
+  sudo -i echo "$pkg" >>packages/hardware-specific
 done
 
 # Add options to install lists
