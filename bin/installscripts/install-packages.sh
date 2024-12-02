741b748a27a389aaad0cdaa74db02836a7959953
Trevor Baughn <trevor.baughn@protonmail.com>
2024-12-01 18:23:12 -0800

    Update install-packages.sh

--- bin/installscripts/install-packages.sh
+++ bin/installscripts/install-packages.sh
@@ -14,7 +14,7 @@ root_password=$11
 # Non-passed-in variables
 package_lists_path=$HOME/bin/installscripts/packages/
 flatpak_lists=(end-user-flatpaks flatpak-runtimes-and-compatibility)
-pacman_lists=(minimal cli-tools audio fonts themes programming-languages desktop-environment hardware-specific)
+pacman_lists=(cli-tools audio fonts themes programming-languages desktop-environment hardware-specific)
 
 #Ignore history for commands that need password piped in
 export HISTIGNORE='*sudo -S*'
